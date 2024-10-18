8eb07e326704e3b16138295654d3c8de61985d1f
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2024-10-19 10:38:26 +1100

    update models

--- group_assignment/code/training/train.py
+++ group_assignment/code/training/train.py
@@ -1,14 +1,21 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+def train(model_name):
+    model = YOLO(f'{model_name}.pt')
 
-# Train the model with 2 GPUs
-results = model.train(
-    data='/mnt/e/Projects/COS40007-Artificial-Intelligence-for-Engineering/group_assignment/datasets/data/data/rubbish/YOLODataset/dataset.yaml', 
-    epochs=100, 
-    imgsz=640,
-    batch=32,
-    project='group_assignment/runs/train',
-    name=f'yolo11n_100_epochs'                   
-)
+    # Train the model with 2 GPUs
+    results = model.train(
+        data='/mnt/e/Projects/COS40007-Artificial-Intelligence-for-Engineering/group_assignment/datasets/data/data/rubbish/YOLODataset/dataset.yaml', 
+        epochs=150, 
+        imgsz=640,
+        batch=32,
+        project='group_assignment/runs/train',
+        name=f'{model_name}_150_epochs',
+        augment=True                   
+    )
+
+yolo11n = train('yolo11n')
+yolo11s = train('yolo11s')
+yolo11m = train('yolo11m')
+yolo11l = train('yolo11l')
