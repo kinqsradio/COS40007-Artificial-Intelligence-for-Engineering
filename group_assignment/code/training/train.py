8234b02396937413216e82c1e18774cbc3a44683
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2024-10-21 14:49:47 +1100

    update models

--- group_assignment/code/training/train.py
+++ group_assignment/code/training/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-def train(model_name, augment=False):
+def train(model_name, epochs=150, augment=False):
     model = YOLO(f'{model_name}.pt')
     
     if augment:
@@ -12,16 +12,20 @@ def train(model_name, augment=False):
         name='original_dataset'
         
     
-    results = model.train(
+    return model.train(
         data=f'/mnt/e/Projects/COS40007-Artificial-Intelligence-for-Engineering/group_assignment/datasets/data/data/rubbish/{dataset}/dataset.yaml', 
-        epochs=150, 
+        epochs=epochs, 
         imgsz=640,
         batch=32,
         project='group_assignment/runs/train',
-        name=f'{model_name}_150_epochs_{name}',
+        name=f'{model_name}_{epochs}_epochs_{name}',
         augment=True                   
     )
 
-yolo11n = train('yolo11n', augment=True)
-yolo11s = train('yolo11s', augment=True)
-yolo11m = train('yolo11m', augment=True)
+yolo11n_original = train('yolo11n', epochs=150, augment=False)
+yolo11s_original = train('yolo11s', epochs=150, augment=False)
+yolo11m_original = train('yolo11m', epochs=150, augment=False)
+
+yolo11n_augment = train('yolo11n', epochs=150, augment=True)
+yolo11s_augment = train('yolo11s', epochs=150, augment=True)
+yolo11m_augment = train('yolo11m', epochs=150, augment=True)
