ac29760900d9e78a3b4e1b67c3587c299f8aa146
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2024-10-21 11:36:03 +1100

    Update Assignment

--- group_assignment/code/training/train.py
+++ group_assignment/code/training/train.py
@@ -1,20 +1,27 @@
 from ultralytics import YOLO
 
 # Load a model
-def train(model_name):
+def train(model_name, augment=False):
     model = YOLO(f'{model_name}.pt')
-
-    # Train the model with 2 GPUs
+    
+    if augment:
+        dataset='Augment_Dataset'
+        name='augment_dataset'
+    else:
+        dataset='Dataset'
+        name='original_dataset'
+        
+    
     results = model.train(
-        data='/mnt/e/Projects/COS40007-Artificial-Intelligence-for-Engineering/group_assignment/datasets/data/data/rubbish/Dataset/dataset.yaml', 
+        data=f'/mnt/e/Projects/COS40007-Artificial-Intelligence-for-Engineering/group_assignment/datasets/data/data/rubbish/{dataset}/dataset.yaml', 
         epochs=150, 
         imgsz=640,
         batch=32,
         project='group_assignment/runs/train',
-        name=f'{model_name}_150_epochs_augment_dataset',
+        name=f'{model_name}_150_epochs_{name}',
         augment=True                   
     )
 
-yolo11n = train('yolo11n')
-yolo11s = train('yolo11s')
-yolo11m = train('yolo11m')
+yolo11n = train('yolo11n', augment=True)
+yolo11s = train('yolo11s', augment=True)
+yolo11m = train('yolo11m', augment=True)
